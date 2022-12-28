f1c7e85605c283724aee4525aaaa96ed86c8235f
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2022-12-28 08:42:06 +0530

    cal

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your code here
+let input=document.getElementById("input");
+
+function addme(data){
+   input.value+=data;
+}
+function ans(){
+   let result=eval(input.value);
+   input.value=result;
+}
+function reset(){
+   input.value="";
+}
